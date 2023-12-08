4cbae94102025a351bab3ae3fb348555a21425da
rishaleva <rishaleva@yandex.ru>
2023-12-08 13:38:38 +0300

    fix problem with alter table

--- src/main/java/ru/rishaleva/springtask/model/User.java
+++ src/main/java/ru/rishaleva/springtask/model/User.java
@@ -2,9 +2,10 @@ package ru.rishaleva.springtask.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
@@ -14,7 +15,7 @@ public class User {
     private long id;
     @Column(name = "name")
     private String name;
-    @Column(name = "lastName")
+    @Column(name = "last_name")
     private String lastName;
     @Column(name = "age")
     private byte age;
